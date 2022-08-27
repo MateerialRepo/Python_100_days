d1a6f3f93112676c272c2ad74c2031a22d144033
Luqman Bello <63483816+MateerialRepo@users.noreply.github.com>
2022-08-27 17:48:55 +0100

    Update conditional_flow_exercise.py
    Increased the complexity for the if else control flow practice for three variables

--- conditional_flow_exercise.py
+++ conditional_flow_exercise.py
@@ -1,6 +1,27 @@
 print("Welcome to the rollercoaster!")
+total_cost = 0
 height = int(input("What is your height in cm? "))
 if height > 120:
   print("You can ride the rollercoaster")
+  age = int(input("Please enter your current age? "))
+  if age<12:
+    print("Please pay $5 for your ticket")
+    total_cost = 5
+  elif age <= 18:
+    print("Please pay $7 for your ticket")
+    total_cost = 7
+  else:
+    print("Please pay $12 for your ticket")
+    total_cost = 12
+
+  photos = input("do you want photos? yes or no? ")
+
+  if photos.lower() == "yes":
+    total_cost = total_cost+3
+  else:
+    total_cost
+  
 else:
   print("Sorry, but you have to grow taller to be able to ride the rollercoaster")
+
+print(f"Your total fees is ${total_cost}")
