a5de766046db8047626c589bbb9e5d9471a8aeba
MateerialRepo <belloolaluqman@gmail.com>
2023-02-09 05:04:45 +0000

    Randomly Picking who pays the bill

---
+++ who_pays_the_bill.py
@@ -0,0 +1,10 @@
+import random
+
+# this s a program to choose who pays the bill from a list of random names
+
+names = input("Please enter everybody's names separated by a comma ',': ")
+name_list = names.split(",")
+random_int = random.randint(0, (len(name_list) - 1))
+who_pays = name_list[random_int]
+
+print(f"{who_pays} is going to pay for today's meal")
