ebc34d3d41040ae4d3249e6300c6bb4ce700a19a
MateerialRepo <belloolaluqman@gmail.com>
2023-02-09 05:07:15 +0000

    modified the code to use the shorter random.choice() syntax

--- who_pays_the_bill.py
+++ who_pays_the_bill.py
@@ -4,7 +4,8 @@ import random
 
 names = input("Please enter everybody's names separated by a comma ',': ")
 name_list = names.split(",")
-random_int = random.randint(0, (len(name_list) - 1))
-who_pays = name_list[random_int]
+# random_int = random.randint(0, (len(name_list) - 1))
+# who_pays = name_list[random_int]
+who_pays = random.choice(name_list)
 
 print(f"{who_pays} is going to pay for today's meal")
