125e53810e63a7b5f9beea2599c8a5928a7d5673
Luqman Bello <63483816+MateerialRepo@users.noreply.github.com>
2023-06-20 12:03:21 +0100

    Update caesar-cipher.py
    Added loop for better user experience

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -1,9 +1,5 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
 def caesar(input_text, shift_num, action):
   result = ""
   if (action == 'decode'):
@@ -17,7 +13,17 @@ def caesar(input_text, shift_num, action):
     result += alphabet[new_position]
   print(f"The {action}d text is {result}")
   
- if shift > 26:
-  shift = shift%26
   
- caesar(input_text=text, shift_num=shift, action=direction)
+should_continue = True
+while should_continue:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  if shift > 26:
+    shift = shift%26
+
+  caesar(input_text=text, shift_num=shift, action=direction)
+  
+  check = input("Type 'yes' if you want to go again. Otherwise, type 'no': \n").lower()
+  if check == "no":
+    should_continue = False
